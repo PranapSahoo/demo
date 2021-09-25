7130b53c540ae6a6bc66705c8408b64b64c4671e
PranapSahoo <53363774+PranapSahoo@users.noreply.github.com>
2021-09-26 00:58:17 +0530

    first commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class   DemoApplication {
 
 	public static void main(String[] args) {
+		/* Added for first commit */
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
